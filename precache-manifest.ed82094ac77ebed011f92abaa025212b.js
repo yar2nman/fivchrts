b4d1c4097b1b56302ef990ff1751f5629773109e
Ahmed7779 <yar2nman@gmail.com>
2022-04-02 12:48:59 +0300

    Updates

---
+++ precache-manifest.ed82094ac77ebed011f92abaa025212b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98ac63f43285792955bfdc6b79a8af1f",
+    "url": "/fivchrts/index.html"
+  },
+  {
+    "revision": "468fb96c51066b476fa1",
+    "url": "/fivchrts/static/css/main.0d2ed91d.chunk.css"
+  },
+  {
+    "revision": "6503981e9e953e010f54",
+    "url": "/fivchrts/static/js/2.b266f8e1.chunk.js"
+  },
+  {
+    "revision": "52fc661712429cc9af7b9bed5de064e2",
+    "url": "/fivchrts/static/js/2.b266f8e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84535d8b9aba7efc2147",
+    "url": "/fivchrts/static/js/3.12a8629e.chunk.js"
+  },
+  {
+    "revision": "468fb96c51066b476fa1",
+    "url": "/fivchrts/static/js/main.6fe2622b.chunk.js"
+  },
+  {
+    "revision": "4511bbfb4cdca6c818d6",
+    "url": "/fivchrts/static/js/runtime-main.1b5bdb9c.js"
+  }
+]);
